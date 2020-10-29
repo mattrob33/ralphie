552f4dcab49e41e6b18796b4243ef30f82802467
Matt Robertson <mattrobertson333@gmail.com>
2020-10-29 09:18:53 -0500

    Added Alberti cipher with test coverage

---
+++ ralphie/src/main/java/com/ralphie/AlbertiCipher.kt
@@ -0,0 +1,100 @@
+package com.ralphie
+
+/**
+ * Encodes and decodes text using an Alberti Cipher, where letters are encoded
+ * according to a given key, historically existing in the form of an Alberti disk.
+ */
+class AlbertiCipher {
+
+    // The lookup table of encoded chars
+    private val albertiDisk: HashMap<Char, Char> = hashMapOf()
+
+    // The reverse lookup table, used for decoding chars
+    private val reverseAlbertiDisk: HashMap<Char, Char> = hashMapOf()
+
+    init {
+        val encodedUpperChars = ('A'..'Z').toMutableList().apply { shuffle() }
+
+        encodedUpperChars.forEachIndexed { index, encodedUpperChar ->
+            val encodedLowerChar = encodedUpperChar + 32
+
+            val upperChar = (65 + index).toChar()
+            val lowerChar = (97 + index).toChar()
+
+            albertiDisk[upperChar] = encodedUpperChar
+            albertiDisk[lowerChar] = encodedLowerChar
+
+            reverseAlbertiDisk[encodedUpperChar] = upperChar
+            reverseAlbertiDisk[encodedLowerChar] = lowerChar
+        }
+
+        albertiDisk[' '] = ' '
+        reverseAlbertiDisk[' '] = ' '
+    }
+
+    /**
+     * Encodes text using an Alberti Cipher.
+     *
+     * @param msg The text to encode. Must consist only of latin alphabet characters (a-z or A-Z) and whitespaces.
+     *
+     */
+    fun encode(msg: String): String {
+        require(isLatinAlphabet(msg)) {
+            "Message must contain only latin alphabet characters (a-z or A-Z)"
+        }
+
+        val encoded = StringBuilder()
+
+        msg.forEach { c ->
+            val encodedChar = encodeChar(c)
+            encoded.append(encodedChar)
+        }
+
+        return encoded.toString()
+    }
+
+    /**
+     * Decodes text using an Alberti Cipher.
+     *
+     * @param encodedMsg The encoded text to decode.
+     *
+     */
+    fun decode(encodedMsg: String): String {
+        require(isLatinAlphabet(encodedMsg)) {
+            "Message must contain only latin alphabet characters (a-z or A-Z)"
+        }
+
+        val decoded = StringBuilder()
+
+        encodedMsg.forEach { c ->
+            val decodedChar = decodeChar(c)
+            decoded.append(decodedChar)
+        }
+
+        return decoded.toString()
+    }
+
+    // TODO: ensure disk is sufficiently scrambled
+    private fun createAlbertiDisk(): HashMap<Char, Char> {
+        val disk: HashMap<Char, Char> = hashMapOf()
+
+        val scrambled = ('A'..'Z').toMutableList().apply { shuffle() }
+
+        scrambled.forEachIndexed { index, char ->
+            disk[(65 + index).toChar()] = char
+            disk[(97 + index).toChar()] = char + 32
+        }
+
+        disk[' '] = ' '
+
+        return disk
+    }
+
+    private fun encodeChar(c: Char): Char {
+        return albertiDisk[c] ?: throw IllegalArgumentException("Illegal char $c")
+    }
+
+    private fun decodeChar(c: Char): Char {
+        return reverseAlbertiDisk[c] ?: throw IllegalArgumentException("Illegal char $c")
+    }
+}
---
+++ ralphie/src/test/java/com/ralphie/AlbertiTest.kt
@@ -0,0 +1,26 @@
+package com.ralphie
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class AlbertiTest {
+
+    private lateinit var alberti: AlbertiCipher
+    
+    @Before
+    fun setup() {
+        alberti = AlbertiCipher()
+    }
+    
+    @Test
+    fun `Decode matches original, with explicit positive shift`() {
+        val msg = "ABCDEFGHIJKLMNOPQRSTUVWYZ abcdefghijklmnopqrstuvwxyz"
+
+        val encoded = alberti.encode(msg)
+        val decoded = alberti.decode(encoded)
+
+        assertNotEquals(encoded, decoded)
+        assertEquals(msg, decoded)
+    }
+}
